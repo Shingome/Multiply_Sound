8b94cf2a0e0f9299db81b26a8c4703de842ccff3
Shingome <user@ubuntu22-lenovo>
2024-05-23 16:53:27 +0300

    add iface

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
+import gradio as gr
 from pydub import AudioSegment
+from src.multiply_audio import multiply_audio
 
 
-def multiply_audio(audio: AudioSegment, count: int, offset: int):
-    audio_result = AudioSegment.silent(audio.duration_seconds * 1000 + offset * count)
-    for i in range(count):
-        print(f"{round(((i + 1) / count) * 100, 1)}%")
-        audio_result = audio_result.overlay(audio, position=i * offset)
+def sentence_builder(count: int, offset: float):
+    file = "tmp/tmp.mp3"
+    multiply_audio(raw_audio, count, offset * 1000.).export(file)
+    return file
 
-    return audio_result
 
+demo = gr.Interface(
+    sentence_builder,
+    [
+        gr.Slider(1, 1000, step=1, value=1, label="Count", info="Shkibididobsteeeep"),
+        gr.Slider(0, 5, step=0.05, value=0, label="Offset", info="Sec between Shkibididobsteeeep")
+    ],
+    "audio"
+)
+
+raw_audio = AudioSegment.from_file("sound.mp3")
 
 if __name__ == "__main__":
-    raw_audio = AudioSegment.from_file("sound.mp3")
-
-    combined_audio = multiply_audio(raw_audio, 10, 1000)
-
-    combined_audio.export("combined_audio.mp3", format="mp3")
+    demo.launch()
